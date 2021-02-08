707685b64983fef285169ce89f917debfbfaf49a
Georgia Harrison <georgiaharrison1994@gmail.com>
2021-02-08 15:49:15 +0000

    Challenge 02-01

---
+++ 02/01-join-words.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+
+function joinWords($firstWord, $secondWord) {
+    return $firstWord . " " . $secondWord;
+};
+
+dump(joinWords("fish", "sticks")); // "fish sticks"
+dump(joinWords("hello", "world")); // "hello world"
+
+?>
