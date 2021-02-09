268863753f35cd2b7d74069209cc13d8cc7db9d3
Georgia Harrison <georgiaharrison1994@gmail.com>
2021-02-09 12:16:44 +0000

    Challenge 03-04

---
+++ 03/04-phone-number.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+//Create a function phoneNumber, that returns true if the given string contains only numbers and hyphens
+
+function phoneNumber($str) {
+    return preg_match("/^[\d\-]+$/", $str) === 1;
+}
+
+dump(phoneNumber("blah")); // false
+dump(phoneNumber("12-323-5-344")); // true
+dump(phoneNumber("12-34-a334-43")); // false
+dump(phoneNumber("--123----23123")); // true
